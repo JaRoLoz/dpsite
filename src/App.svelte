74534d9911b3b8196bac2ca8f9e3f464dd797953
Er Zurky Nene <40668121+JaRoLoz@users.noreply.github.com>
2022-11-28 13:17:03 +0000

    Topbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,19 @@
-<h1>r</h1>
+<script lang="ts">
+    import Topbar from "./components/Topbar.svelte";
+</script>
+
+<Topbar />
+<div id="page-content">
+    <div>r</div>
+    <div id="about-content">r</div>
+</div>
+
+<style>
+    #page-content {
+
+    }
+    #about-content {
+        width: 20rem;
+        background-color: aqua;
+    }
+</style>
---
+++ src/components/Topbar.svelte
@@ -0,0 +1,87 @@
+<script>
+
+</script>
+
+<div>
+    <div id="top-info">
+        <div id="visit-counter">
+            <span>6</span>
+            <span>9</span>
+            <span>6</span>
+            <span>9</span>
+        </div>
+        <div>
+            <h1><span>Web 2.0</span></h1>
+        </div>
+    </div>
+    <div id="top-link">
+        <a href="#/"><span>Inicio</span></a>
+        <a href="#/"><span>Página 1</span></a>
+        <a href="#/"><span>Página 2</span></a>
+        <a href="#/"><span>Página 3</span></a>
+        <a href="#/"><span>Página 4</span></a>
+    </div>
+</div>
+
+<style>
+    h1 {
+        font-size: 35px;
+    }
+
+    div:first-of-type {
+        color: rgba(255, 255, 255, 0.87);
+        background-color: #242424;  
+    }
+
+    #top-info {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        border-bottom: 1px solid black;
+        align-items: center;
+        place-items: center;
+        min-height: 4rem;
+    }   
+
+    #visit-counter {
+        display: flex;
+        align-items: center;
+        font-size: 1.4rem;
+        border: 2px solid rgba(255, 255, 255, 0.87);
+        border-radius: 10px;
+    }
+
+    #visit-counter > span {
+        padding: 2px;
+        border-left: 2px solid rgba(255, 255, 255, 0.87);
+    }
+
+    #visit-counter > span:first-child {
+        border: none;
+    }
+
+    #top-link {
+        min-height: 2rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        padding: 4px;
+        border-bottom: 1px solid black;
+    }
+
+    a {
+        border-radius: 10px;
+        color:rgba(255, 255, 255, 0.87);
+        border: none;
+        transition: .3s;
+    }
+
+    a:hover {
+        border: 2px solid rgba(255, 255, 255, 0.87);
+        padding: 3px;
+        width: 6rem;
+        display: flex;
+        flex-direction: columns;
+        justify-content: center;
+        transition: .3s;
+    }
+</style>
